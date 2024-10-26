5e0343f3590fd10dfb232fd84af86052a653d900
Whale <dkdk137882@gmail.com>
2024-10-26 04:50:48 +0000

    sys모듈/sys1클래스/add,mul메서드

---
+++ sys/sys1.py
@@ -0,0 +1,12 @@
+class sys1:
+    def __init__(self, first, second):
+        self.second = second
+        self.first = first
+    def add(self):
+    	result = self.first + self.second
+        return result
+    def mul(self):
+        result = self.first * self.second
+        return result
+    
+    
